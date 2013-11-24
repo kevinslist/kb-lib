aa3a5757731876954c401313d06acbfe4583477b
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2013-11-24 06:38:12 +0000

    enhance the lib

--- third-party/core/kb_controller.php
+++ third-party/core/kb_controller.php
@@ -5,7 +5,7 @@ if (!defined('BASEPATH')){  exit('No direct script access allowed'); }
 class kb_controller extends CI_Controller {
   
   public function render_page(){
-    print $this->load->view('layouts/default');
+    print $this->load->view('layouts/default_layout');
   }
 
 
