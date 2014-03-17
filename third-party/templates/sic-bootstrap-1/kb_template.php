dd6ae423b3d5917633b60f1852b61486d8a27307
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2014-03-17 05:50:08 +0000

    add phue lib

---
+++ third-party/templates/sic-bootstrap-1/kb_template.php
@@ -0,0 +1,21 @@
+<?php
+
+function kb_template_get_css(){
+  return array(
+    'assets/kb/templates/sic-bootstrap-1/lib/bootstrap3/css/bootstrap.css',
+    'assets/kb/templates/sic-bootstrap-1/lib/jquery/layout/1.3/jquery.layout.css',
+    'assets/kb/templates/sic-bootstrap-1/form/css/formalize/kb.bootstrap.formalize.css',
+    'assets/kb/templates/sic-bootstrap-1/form/css/kb.form-global.css',
+  );
+}
+function kb_template_get_js(){
+  return array(
+    'assets/kb/templates/sic-bootstrap-1/lib/jquery/1.10/js/jquery.js',
+    'assets/kb/templates/sic-bootstrap-1/lib/bootstrap3/js/bootstrap.min.js',
+    'assets/kb/templates/sic-bootstrap-1/lib/jquery/1.10/js/jquery.ui.js',
+    'assets/kb/templates/sic-bootstrap-1/lib/jquery/layout/1.3/jquery.layout.js',
+    'assets/kb/templates/sic-bootstrap-1/form/js/form.js',
+    'assets/kb/templates/sic-bootstrap-1/js/kb-auth.js',
+    
+  );
+}
---
+++ third-party/templates/sic-bootstrap-1/views/assets/js.php
@@ -0,0 +1,3 @@
+<?php foreach($js_files as $j): ?>
+<script src="<?= site_url($j) ?>"></script>
+<?php endforeach; ?>
