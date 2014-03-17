4f96ca20f5a40712cdf48b528db12617f42f168a
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2014-03-17 05:51:51 +0000

    allow lowercase

--- trunk/third-party/core/kb_client.php
+++ trunk/third-party/core/kb_client.php
@@ -1,38 +1,75 @@
 <?php
 
-        
-class kb_client{
+define('KB_CLIENT_IS_LOGGED_IN', 'kb_client_is_logged_in');
 
-  private $bsg_auth = FALSE;
-  var $bsg_auth_roles = array();
-  var $bsg_auth_permissions = array();
+class kb_client {
+
+  var $id = NULL;
+  var $roles = array();
   var $permissions = array();
-  var $unauthenticated_name = 'Guest';
-  
+  var $name = 'Guest';
+  var $email = NULL;
+  var $status = 'guest';
+
   public function __construct() {
     if (!kb::is_cron()) {
       ini_set('session.cookie_domain', $_SERVER['HTTP_HOST'] . '; HttpOnly');
       ini_set('session.gc_maxlifetime', 72000);
       ini_set('session.cookie_lifetime', 0);
       ini_set('session.cookie_secure', 'On');
+      $this->set_info_from_session();
+    }
+  }
+
+  public function set_info_from_session($do_lookup = FALSE) {
+    $kb_client_info = $this->session_data('kb-client-info');
+    if (!empty($kb_client_info)) {
+      $this->id = $kb_client_info['id'];
+      $this->name = $kb_client_info['name'];
+      $this->email = $kb_client_info['email'];
+      $this->status = $kb_client_info['email'];
+      $this->set_authorization($do_lookup);
+    }
+  }
+
+  public function set_authorization($force_new_lookup = FALSE) {
+    $this->set_roles($force_new_lookup);
+    $this->set_permissions($force_new_lookup);
+  }
+
+  public function set_roles($force_new_lookup = NULL) {
+    $this->roles = empty($this->session_data('kb-client-info-roles')) ? array() : $this->session_data('kb-client-info-roles');
+    if ($force_new_lookup) {
+      $this->roles = kb::db_values('SELECT role_id FROM user_roles WHERE user_id = ?', $this->id);
+      $this->session_data('kb-client-info-roles', $this->roles);
+    }
+  }
+
+  public function set_permissions($do_lookup = FALSE) {
+    $permissions = empty($this->session_data('kb-client-info-permissions')) ? array() : $this->session_data('kb-client-info-permissions');
+    if ($force_new_lookup) {
+      $permissions_direct = kb::db_values('SELECT permission_id FROM user_permissions WHERE user_id = ?', $this->id);
+      $permissions_roles = empty($this->roles) ? array() : kb::db_values('SELECT permission_id FROM roles_permissions
+                                          WHERE role_id IN (\'' . implode("', '", array_keys($this->roles)) . '\')');
+      $this->permissions = array_merge($permissions_direct, $permissions_roles);
+      $this->session_data('kb-client-info-permissions', $this->permissions);
     }
   }
 
   public function logged_in($set_logged_in = null) {
     $return = $this;
-
     if (is_null($set_logged_in)) {
-      $return = isset($_SESSION[BSG_CLIENT_DID_AUTH_INIT]) && $_SESSION[BSG_CLIENT_DID_AUTH_INIT];
+      $return = isset($_SESSION[KB_CLIENT_IS_LOGGED_IN]) && $_SESSION[KB_CLIENT_IS_LOGGED_IN];
     } else {
-      $_SESSION[BSG_CLIENT_DID_AUTH_INIT] = $set_logged_in;
-      if(FALSE === $set_logged_in){
+      $_SESSION[KB_CLIENT_IS_LOGGED_IN] = $set_logged_in;
+      if (FALSE === $set_logged_in) {
         $this->log_out();
       }
     }
     return $return;
   }
 
-  public function log_out(){
+  public function log_out() {
     $this->load->library('session');
     $this->session->sess_destroy();
     $_SESSION = array();
@@ -43,6 +80,7 @@ class kb_client{
     $this->displayname = $this->unauthenticated_name;
   }
 
+
   public function initzz($auid = null, $uupic = null) {
 
     if (!kb::is_cron()) {
@@ -52,12 +90,12 @@ class kb_client{
       $auid = $this->get_auid_from_server();
       if ($this->logged_in()) {
         $this->set_info_from_session();
-        if($auid != $this->auid){
+        if ($auid != $this->auid) {
           $this->log_out();
           $this->init($auid);
         }
-      } else{
-        if(!empty($auid)){
+      } else {
+        if (!empty($auid)) {
           $this->init($auid);
         }
       }
@@ -77,52 +115,7 @@ class kb_client{
     }
   }
 
-  function set_info_from_session() {
-    $ned_info = $this->session_data('ned-info');
-    $this->firstname = $ned_info['firstname'];
-    $this->lastname = $ned_info['lastname'];
-    $this->uupic = $ned_info['uupic'];
-    $this->auid = $ned_info['auid'];
-    $this->email = $ned_info['email'];
-    $this->phone = empty($ned_info['phone']) ? NULL : $ned_info['phone'];
-    $this->orgcode = $ned_info['orgcode'];
-    $this->nasaidentitystatus = $ned_info['nasaidentitystatus'];
-    $this->center = $ned_info['center'];
-    $this->displayname = $ned_info['shortdisplayname'];
-    $this->arnumber = $this->x500id = $ned_info['x500id'];
-    $this->building = isset($ned_info['building']) ? $ned_info['building'] : NULL;
-    $this->room = isset($ned_info['room']) ? $ned_info['room'] : NULL;
-    $this->temp_perm = isset($ned_info['temp_perm']) ? $ned_info['temp_perm'] : NULL;
-    $this->foreign_national = isset($ned_info['foreign_national']) ? $ned_info['foreign_national'] : NULL;
-    $this->citizenship = isset($ned_info['citizenship']) ? $ned_info['citizenship'] : NULL;
-
-    if (bsg::is_dev() || bsg::is_stage()) {
-      $this->bsg_auth_settings();
-    }
-    $bsg_auth_roles = $this->session_data('bsg-auth-roles');
-    $bsg_auth_permissions = $this->session_data('bsg-auth-permissions');
-    $this->bsg_auth = $this->session_data('bsg-auth');
-    $this->bsg_auth_roles = !empty($bsg_auth_roles) ? $bsg_auth_roles : $this->bsg_auth_roles;
-    $this->bsg_auth_permissions = !empty($bsg_auth_permissions) ? $bsg_auth_permissions : $this->bsg_auth_permissions;
-  }
-
   /* DEPRECIATED */
-
-  function set_permissions() {
-    // define $this->session_data('permissions') in your local client file. each application may vary. permissions should be
-    // an array of roles
-    if ($this->session_data('permissions')) {
-      $this->permissions = $this->session_data('permissions');
-    }
-  }
-
-  function display_name() {
-    return empty($this->displayname) ? $this->unauthenticated_name : $this->displayname;
-  }
-
-  function get_uupic() {
-    return empty($this->uupic) ? null : $this->uupic;
-  }
 
   function get_navigation() {
     $nav_items = array();
@@ -162,10 +155,10 @@ class kb_client{
       $_SESSION['user_messages'][$type][] = $clean_message;
     }
   }
-  
-  public function get_message_section(){
+
+  public function get_message_section() {
     $m = $this->get_messages();
-    return  empty($m) ? NULL : bsg::view('common/user_messages', array('messages' => $m));
+    return empty($m) ? NULL : bsg::view('common/user_messages', array('messages' => $m));
   }
 
   public static function get_messages() {
@@ -202,11 +195,11 @@ class kb_client{
   public function can($privelege = NULL, $redirect = FALSE, $p = NULL) {
     $allowed = FALSE;
 
-    if('bsg-auth-access-application' == $privelege){
-      if(in_array($privelege . '-' . $this->nams_id, $this->bsg_auth_permissions)){
+    if ('bsg-auth-access-application' == $privelege) {
+      if (in_array($privelege . '-' . $this->nams_id, $this->bsg_auth_permissions)) {
         $allowed = TRUE;
       }
-    }else{
+    } else {
       if ($this->bsg_auth) {
         switch ($privelege) {
           default:
@@ -242,90 +235,13 @@ class kb_client{
     }
   }
 
-  function bsg_auth_settings() {
-    $settings = bsg::config('bsg_auth');
-    if (!empty($settings)) {
-      $this->bsg_auth = isset($settings['bsg-auth']) && $settings['bsg-auth'] ? TRUE : FALSE;
-      $this->nams_id = isset($settings['nams-id']) && !empty($settings['nams-id']) ? $settings['nams-id'] : null;
-      $this->nams_id_required = isset($settings['nams-id-required']) && TRUE === $settings['nams-id-required'] ? TRUE : FALSE;
-
-      if ($this->bsg_auth) {
-        $this->bsg_auth_set_roles();
-        $this->bsg_auth_set_permissions();
-      }
-      if (!empty($this->nams_id)) {
-        $nasa_apps_apache = getenv('NASAUserApplications');
-        $nasa_apps_apache = $nasa_apps_apache ? $nasa_apps_apache : '';
-        $nasa_apps_server = isset($_SERVER['NASAUserApplications']) ? $_SERVER['NASAUserApplications'] : '';
-        $nasa_apps = strlen($nasa_apps_apache) > strlen($nasa_apps_server) ? $nasa_apps_apache : $nasa_apps_server;
-
-        if(is_string($nasa_apps) && strlen($nasa_apps)) {
-          $idmax_accounts = explode(';', $nasa_apps);
-          if (!empty($idmax_accounts)) {
-            foreach ($idmax_accounts as $id) {
-                $app_key = $id;
-                $this->bsg_auth_permissions['bsg-auth-access-application-' . $app_key] = 'bsg-auth-access-application-' . $app_key;
-              if (preg_match('`^[0-9]+$`', $id)) {
-              }
-            }
-            $this->session_data('bsg-auth-permissions', $this->bsg_auth_permissions);
-          }
-        }
-      }
-    }
-    $this->session_data('bsg-auth', $this->bsg_auth);
-  }
-  
-  function bsg_auth_set_roles() {
-    // 1) use same database - or 2) define new config for shared DB (option 1 for now)
-
-    $roles = bsg::db_array('SELECT ar.auth_role_key, ar.auth_role_label
-                              FROM auth_user_role aur
-                              LEFT JOIN auth_role ar ON aur.auth_role_id=ar.auth_role_id
-                              WHERE aur.auth_user_uupic=?', $this->uupic);
-    foreach ($roles as $role) {
-      $this->bsg_auth_roles[$role['auth_role_key']] = $role['auth_role_label'];
-    }
-
-
-    $this->session_data('bsg-auth-roles', $this->bsg_auth_roles);
-  }
-
-  function bsg_auth_set_permissions() {
-    // 1) use same database - or 2) define new config for shared DB (option 1 for now)
-    // get permissions from direction user->permission assignments
-    $permissions = bsg::db_values('SELECT ap.auth_permission_key
-                              FROM auth_user_permission aup
-                              LEFT JOIN auth_permission ap ON aup.auth_permission_id=ap.auth_permission_id
-                              WHERE aup.auth_user_uupic=?', $this->uupic);
-    foreach ($permissions as $permission) {
-      $this->bsg_auth_permissions[$permission] = $permission;
-    }
-    if (count($this->bsg_auth_roles)) {
-      // get permissions from user->role->permission assignments
-      $sql = 'SELECT ap.auth_permission_key
-                                FROM auth_role_permission arp
-                                LEFT JOIN auth_permission ap ON arp.auth_permission_id=ap.auth_permission_id
-                                LEFT JOIN auth_role ar ON arp.auth_role_id=ar.auth_role_id
-                                WHERE ar.auth_role_key IN (\'' . implode("', '", array_keys($this->bsg_auth_roles)) . '\')';
-
-
-      $permissions = bsg::db_values($sql);
-      foreach ($permissions as $permission) {
-        $this->bsg_auth_permissions[$permission] = $permission;
-      }
-    }
-    $this->session_data('bsg-auth-permissions', $this->bsg_auth_permissions);
-  }
-
-  function redirect_saml_login($redirect_url = NULL){
+  function redirect_saml_login($redirect_url = NULL) {
     $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
     $web_root = $saml_login_url = 'https://' . $_SERVER['SERVER_NAME'];
     $target = empty($redirect_url) ? $web_root . $request_uri : $redirect_url;
     $saml_login_url = $web_root . '/Shibboleth.sso/Login?target=' . $target;
     redirect($saml_login_url);
     die();
-
   }
 
 }
--- trunk/third-party/core/kb_loader.php
+++ trunk/third-party/core/kb_loader.php
@@ -1,15 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class kb_loader extends CI_Loader {
+  public $kb_view_path = array();
+  
   public function __construct() {
     parent::__construct();
-    $this->_ci_view_paths = array_merge( 
-                                        $this->_ci_view_paths,
-                                        array(
-                                              APPPATH . 'third_party/kb/forms/v1/views/'=>TRUE,
-                                              APPPATH . 'third_party/kb/views/'=>TRUE
-                                            ) 
-                                      );
-
+    $this->kb_add_view_path(APPPATH . 'third_party/kb/templates/' . kb::$template_name . '/views/');
+  }
+  public function kb_set_view_path(){
+    $temp = array();
+    foreach($this->kb_view_path as $t){
+      $temp[$t] = true;
+    }
+    $this->_ci_view_paths = array_merge(
+          $this->_ci_view_paths,
+          $temp
+        );
+  }
+  
+  public function kb_add_view_path($p = NULL){
+    if(is_array($p)){
+      $this->kb_view_path = array_merge($this->kb_view_path, $p);
+    }else{
+      $this->kb_view_path[] = $p;
+    }
+    $this->kb_set_view_path();
   }
 }
