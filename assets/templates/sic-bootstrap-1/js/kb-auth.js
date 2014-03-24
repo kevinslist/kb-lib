293cba6418a9b03ced6a9fef0c210f5ec4821931
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2014-03-24 06:07:28 +0000

    google login

--- assets/templates/sic-bootstrap-1/js/kb-auth.js
+++ assets/templates/sic-bootstrap-1/js/kb-auth.js
@@ -1,27 +1,27 @@
 $(document).ready(kb_auth_global_init);
+var kb_auth_type = '';
 var kb_google_client_id = '';
-var kb_google_auth_type = '';
 
 function kb_auth_global_init() {
-  if ($('div.kb-google-auth-load').length > 0) {
-    kb_google_client_id = $('#kb-google-signin-block').attr('data-clientid');
-    kb_google_auth_type = $('#kb-auth-type').attr('data-kb-auth-type');
-    $.getScript('https://apis.google.com/js/client:plusone.js');
+  if ($('div#kb-athentication-provider-wrapper').length > 0) {
+    kb_auth_type = $('div#kb-athentication-provider-wrapper').attr('data-kb-auth-type');
+    if($('#kb-google-signin-block').length){
+      kb_google_client_id = $('#kb-google-signin-block').attr('data-clientid');
+      $.getScript('https://apis.google.com/js/client:plusone.js');
+    }
   }
 }
 
 function kb_google_signin_callback(auth_result) {
   if (auth_result) {
     if (auth_result['status'] != undefined && auth_result['status']['signed_in']) {
-      if ('logout' == kb_google_auth_type) {
+      if ('logout' == kb_auth_type) {
         gapi.auth.signOut();
-        //kb_structure_resize();
       } else {
-        //kb_finalize_login_anime();
-        var access_token = auth_result['access_token'];
-        auth_result['provider'] = 'google';
-        auth_result['access_token'] = access_token;
-        kb_login_check_registration(auth_result);
+        var p = {};
+        p['provider'] = 'google';
+        p['access_token'] = auth_result['access_token'];
+        $('#kb-athentication-provider-wrapper').trigger('kb-login-user-authenticated', p);
       }
     } else {
       var google_params = {
@@ -31,7 +31,6 @@ function kb_google_signin_callback(auth_result) {
         'scope': 'https://www.googleapis.com/auth/plus.me'
       };
       gapi.auth.signIn(google_params);
-
     }
   } else {
     var p = {
@@ -42,21 +41,4 @@ function kb_google_signin_callback(auth_result) {
     gapi.auth.authorize(p, google_signin_callback);
   }
   return;
-}
-
-function kb_login_check_registration(params){
-  params[kb_csrf_name] = kb_csrf_hash;
-  $.ajax({
-    url: app_home + 'login/check-registration-ajax',
-    dataType: 'json',
-    data: params,
-    type: 'POST',
-    success: kb_login_check_registration_response,
-    error: kb_login_check_registration_response
-  });
-}
-
-function kb_login_check_registration_response(r){
-  log('kb_login_check_registration_response:');
-  log(kb_login_check_registration_response);
 }
