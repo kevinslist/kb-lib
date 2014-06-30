8ca000418c6be905e7523b2394e86f5f13251687
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2014-06-30 07:13:32 +0000

    hue updates

--- third-party/templates/sic-bootstrap-1/views/layouts/default_layout.php
+++ third-party/templates/sic-bootstrap-1/views/layouts/default_layout.php
@@ -3,6 +3,7 @@
 <head>
 <title><?=kb::ci()->page_title();?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="<?php echo site_url('favicon.png');?>" />
 <?=$css?>
 <script type="text/javascript">
   var app_home = "<?= site_url() ?>";
