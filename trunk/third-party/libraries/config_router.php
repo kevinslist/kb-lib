b86de7026e3fbd587ee1ce6e986ad90663e30c36
kb@kevinslist.com <kb@kevinslist.com@bdfc2949-129c-d7bb-15de-5f6365f9344e>
2015-02-16 21:53:13 +0000

    add semaphores

--- trunk/third-party/libraries/config_router.php
+++ trunk/third-party/libraries/config_router.php
@@ -2,11 +2,28 @@
 
 class config_router {
   
-  static function route($c){
-    print 'ROUTE NEW REMOTE SIGNAL: ' . PHP_EOL;
-    print_r($c);
-    print PHP_EOL;
+  static function route($signal){
+    //print 'ROUTE NEW REMOTE SIGNAL: ' . PHP_EOL;
+    //print_r($signal);
+    //print PHP_EOL;
+    
+    $current_time = (int) $signal['last-signal'];
+    $remote_code = '#' . $signal['header-string'];
+    $current_signal = $signal['remote-string'];
+
+    if (!$signal['is-repeat']) {
+      itach::$remotes[$remote_code]['repeat'] = 0;
+      itach::$remotes[$remote_code]['previous-signal'] = $current_signal;
+      itach::$remotes[$remote_code]['last-sent'] = $current_time;
+    }
+    itach::send_signal($signal);
+    
+    
   }
   
+  static function route_special(){
+   print 'ROUTE SPECIAL CHECK' . PHP_EOL;
+  
+  }
 
 }
